e75c7132362208f9445423a98bc4bb86b32b6431
Admin <Admin@10.20.27.11>
2017-11-19 08:48:13 -0500

    Testing connection

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,7 @@ public class HelloWorld {
 	
 	    public static void main( String[] args ) {
 	        System.out.println( "fuck!" );
+	        System.out.println("Robert is connected to github with eclipse and keeps talking in third person");
 	        System.exit( 0 ); //success
 	    }
 	/*
