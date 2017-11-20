c85ddb5942d0d2e65c95d8e417d1ad0e9da4d8b0
rcox2 <33792839+rcox2@users.noreply.github.com>
2017-11-19 21:08:36 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,7 +6,4 @@ public class HelloWorld {
 	        System.out.println("Robert is connected to github with eclipse and keeps talking in third person");
 	        System.exit( 0 ); //success
 	    }
-	/*
-	Robert can edit!
-	*/
 }
