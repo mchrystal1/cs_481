4e3cd642255114d0678be000240069a7c436c6ec
mchrystal1 <mchrystal1@gaels.iona.edu>
2018-02-26 21:15:43 -0500

    Book class 2_26 9:15

--- cs_481/Book.java
+++ cs_481/Book.java
@@ -55,7 +55,7 @@ public class Book {
 	public static File bookFile; 
 	public static String title;
 	public static String author;
-	public int age;
+	public static int age;
 	public static int ISBN;
 	//public static Book bookObject;
 	
@@ -63,8 +63,11 @@ public class Book {
 	
 	static ArrayList<Book> booksList = new ArrayList<Book>();
 	
-	public Book(File bookFile, String author, int ISBN, String title) {
-		// TODO Auto-generated constructor stub
+	public Book(File bookFile, String author, int ISBN, String title, int age) {
+		this.setAuthor(author);
+		this.setISBN(ISBN);
+		this.setTitle(title);
+		this.setAge(age);
 	}
 
 
@@ -79,12 +82,25 @@ public class Book {
 		desktop.open(newBook);
 		
 		Scanner reader = new Scanner(System.in);  // Reading from System.in
-		System.out.println("Enter a number: ");
-		int n = reader.nextInt(); // Scans the next token of the input as an int.
-		//once finished
+		System.out.println("Enter the title: ");
+		String title = reader.nextLine(); 
+		System.out.println("Enter the author: ");
+		String author = reader.nextLine(); 
+		System.out.println("Enter the ISBN: ");
+		int ISBN = reader.nextInt(); 
+		System.out.println("Enter the age this book is for: ");
+		int age = reader.nextInt();
 		reader.close();
 		
-		AddBook(newBook, "Mary Smith", 109282, "Book1");
+		System.out.println("Title is " + title + " author is: " + author + " isbn is: " + ISBN + " age is: " + age);
+		
+		AddBook(newBook, author, ISBN, title, age);
+		
+		for (int i = 0; i < booksList.size(); i++) {
+            Book value = booksList.get(i);
+            System.out.println("Element: " + value);
+        }
+		
 		
 	//	book = new File ("C:~/_Software/test2.txt");
 	//	if (book.exists()) desktop.open(file);
@@ -97,40 +113,55 @@ public class Book {
 		this.bookObject = x; 
 		}
 	*/
-	public Book getBook(Book x)
-	{
+	public Book getBook(Book x){
 		return x; 
 	}
-	
 	public void setAuthor(String author){
-		this.author = author;
+		Book.author = author;
 	}
-	public String getAuthor(String author){
-		return author;
+	public String getAuthor(Book x){
+		return Book.author;
 	}
 	public void setTitle(String title){
-		this.title = title;
+		Book.title = title;
 	}
-	public String getTitle(String title){
-		return title;
+	public String getTitle(Book x){
+		return Book.title;
 	}
 	public void setISBN(int ISBN){
-		this.ISBN = ISBN;
+		Book.ISBN = ISBN;
 	}
-	public int getISBN(int ISBN){
-		return  ISBN;
+	public int getISBN(Book x){
+		return  Book.ISBN;
 	}
+	public void setAge(int age) {
+		Book.age = age;
+	}
+	public int getAge (Book x) {
+		return Book.age;
+	}
+	public void setBookFile(File bookfile) {
+		Book.bookFile = bookfile;
+	}
+	public File getBookFile (Book x) {
+		return Book.bookFile;
+	}
+
 	
 	@SuppressWarnings("unused")
-	public static void AddBook(File x,String author,int ISBN,String title ){
-		boolean insystem = CompareBook(x);
+	public static void AddBook(File x,String author,int ISBN,String title, int age ){
+	/*	boolean insystem = CompareBook(x);
 		if (insystem = true){
 			System.out.println("Book already exists");
 		}
+		
 		else
+		*/
+		
 		{
-			Book addingThis = new Book(bookFile, author, ISBN, title);
-			booksList.add(New(addingThis));
+			Book addingThis = new Book(bookFile, author, ISBN, title, age);
+			booksList.add(addingThis);
+			System.out.println(booksList);
 			/*x.setAuthor(author);
 			x.setISBN(ISBN);
 			x.setTitle(title);
@@ -139,21 +170,26 @@ public class Book {
 		}
 	}
 	
-	private static Book New(Book x) {
+	/*private static Book  New(Book x, String author, int ISBN, String title, int age) {
 		
 		// TODO Auto-generated method stub
 		
 		x.setAuthor(author);
 		x.setISBN(ISBN);
 		x.setTitle(title);
-		//x.setBook(x);
+		x.setAge(age);
 		
-		return null;
+		x.getAuthor(x);
+		x.getISBN(x);
+		x.getAge(x);
+		x.getTitle(x);
+		return x;
 	}
-
+*/
 
 	public static boolean CompareBook(File x){
-		
+		//System.out.println(Arrays.toString((booksList));
+	//	Arrays.toString(booksList.toArray());
 			if (booksList.contains(x))
 					{
 						return true;//book in system
